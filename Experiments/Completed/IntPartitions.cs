46e800f6ef3530d056f28287bdd99f561c50680a
qalisander <qalisander@gmail.com>
2021-04-29 23:25:53 +0300

    IntPartitions refactoring 3

--- Experiments/Completed/IntPartitions.cs
+++ Experiments/Completed/IntPartitions.cs
@@ -5,40 +5,22 @@ namespace Experiments.Completed
 {
     public class IntPartitions
     {
-        // private HashSet<int>[] mem { get; }
-
-        // public IntPartitions(long n) => mem = Enumerable.Range(0, (int)n + 1).Select(i => new HashSet<int>() { i }).ToArray(); // TODO: Lookup
-
         public static string Part(long n)
         {
-            var memory = Enumerable.Range(0, (int)n + 1).Select(i => new HashSet<int>() { i }).ToArray(); // TODO: straightforward solution
-            var ans = PartRec((int)n).OrderBy(x => x).ToList();
+            var ans = PartRec((int)n, new int[n + 1][]).OrderBy(x => x).ToArray();
 
-            return $"Range: {ans.Max() - ans.Min()} "
-                   + $"Average: {ans.Average():.00} "
-                   + $"Median: {Median(ans):.00}";
+            return $"Range: {ans.Max() - ans.Min()} Average: {ans.Average():.00} Median: {Median(ans):.00}";
 
-            IEnumerable<int> PartRec(int arg)
-            {
-                if (memory[arg].Count == 1)
-                {
-                    for (var i = 1; i <= arg / 2; i++)
-                        memory[arg].UnionWith(PartRec(arg - i).Select(x => x * i)); // TODO: Concat, Distinct
-                }
-
-                return memory[arg];
-            }
+            IEnumerable<int> PartRec(int arg, int [][] memory) => 
+                memory[arg] ?? (memory[arg] = Enumerable.Range(arg, 1)
+                    .Union(Enumerable.Range(1, arg / 2)
+                        .SelectMany(i => PartRec(arg - i, memory).Select(x => x * i))
+                        .Distinct())
+                    .ToArray());
 
             static double Median(IList<int> list) => list.Count % 2 == 0
                 ? ((double)list[list.Count / 2 - 1] + list[list.Count / 2]) / 2
                 : list[list.Count / 2];
         }
-
-        // public HashSet<int> this[int i]
-        // {
-        //     get {
-                
-        //     };
-        // }
     }
 }
