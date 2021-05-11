219e7fdd8b3debc7b675f47e41544ea609811087
Alisander Qoshqosh <qalisander@gmail.com>
2021-05-12 01:11:53 +0300

    PlayWithTwoStrings

---
+++ Experiments/PlayWithTwoStrings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Experiments
+{
+    public class PlayWithTwoStrings
+    {
+        public string WorkOnStrings(string a, string b)
+        {
+            // rewrite with GroupJoin
+            var mapA = a.GroupBy(ch => char.ToLower(ch)).ToDictionary(ch => ch.Key, ch => ch.Count());
+            var mapB = b.GroupBy(ch => char.ToLower(ch)).ToDictionary(ch => ch.Key, ch => ch.Count());
+
+            //var ans = new StringBuilder();
+
+            //foreach (var ch in a)
+            //{
+            //    if (mapB.TryGetValue(char.ToLower(ch), out var counts) && counts % 2 != 0)
+            //    {
+            //        ans.Append(SwitchCase(ch));
+            //    }
+            //    else
+            //    {
+            //        ans.Append(ch);
+            //    }
+            //}
+
+            //foreach (var ch in b)
+            //{
+            //    if (mapA.TryGetValue(char.ToLower(ch), out var counts) && counts % 2 != 0)
+            //    {
+            //        ans.Append(SwitchCase(ch));
+            //    }
+            //    else
+            //    {
+            //        ans.Append(ch);
+            //    }
+            //}
+
+            //return ans.ToString();
+
+
+            return String.Concat(a.Select(ch =>
+                    mapB.TryGetValue(char.ToLower(ch), out var counts1) && counts1 % 2 != 0
+                        ? SwitchCase(ch)
+                        : ch)
+                .Concat(b.Select(ch =>
+                    mapA.TryGetValue(char.ToLower(ch), out var counts) && counts % 2 != 0
+                        ? SwitchCase(ch)
+                        : ch)));
+        }
+
+        private static char SwitchCase(char ch) => char.IsUpper(ch) ? char.ToLower(ch) : char.ToUpper(ch);
+    }
+}
---
+++ NTests/PlayWithTwoStringsTest.cs
@@ -0,0 +1,31 @@
+﻿using Experiments;
+using NUnit.Framework;
+
+namespace NTests
+{
+    [TestFixture]
+    public class PlayWithTwoStringsTest
+    {
+        [Test]
+        public void PlayWithTwoStrings_withoutRandom1()
+        {
+            Assert.AreEqual("abCCde", new PlayWithTwoStrings().WorkOnStrings("abc","cde"));
+        }
+        [Test]
+        public void PlayWithTwoStrings_withoutRandom2()
+        {
+            Assert.AreEqual("ABABbababa", new PlayWithTwoStrings().WorkOnStrings("abab", "bababa"));
+
+        }
+        [Test]
+        public void PlayWithTwoStrings_withoutRandom3()
+        {
+            Assert.AreEqual("abcDeFGtrzWDEFGgGFhjkWqE", new PlayWithTwoStrings().WorkOnStrings("abcdeFgtrzw", "defgGgfhjkwqe"));
+        }
+        [Test]
+        public void PlayWithTwoStrings_withoutRandom4()
+        {
+            Assert.AreEqual("abcDEfgDEFGg", new PlayWithTwoStrings().WorkOnStrings("abcdeFg", "defgG"));
+        }
+    }
+}
