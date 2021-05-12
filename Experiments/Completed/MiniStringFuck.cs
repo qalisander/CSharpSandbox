645be29c46a8de989e5e53159368148cd9a31294
Alisander Qoshqosh <qalisander@gmail.com>
2021-05-12 04:24:10 +0300

    MiniStringFuck

---
+++ Experiments/Completed/MiniStringFuck.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Experiments.Completed
+{
+    // https://www.codewars.com/kata/586dd26a69b6fd46dd0000c0/train/csharp
+    public class MiniStringFuck
+    {
+        public static string MyFirstInterpreter(string code) =>
+            code.Aggregate((new StringBuilder(), (byte)0), (acml, ch) => ch switch
+            {
+                '+' => (acml.Item1, acml.Item2 += 1),
+                '.' => (acml.Item1.Append((char)acml.Item2), acml.Item2),
+                _ => acml,
+            }).Item1.ToString();
+    }
+}
