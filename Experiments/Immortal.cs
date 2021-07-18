9be4d5257f23b03c322f65d85f9b344420fbcd6f
Alisander Qoshqosh <qalisander@gmail.com>
2021-07-18 03:36:18 +0300

    refactoring

--- Experiments/Immortal.cs
+++ Experiments/Immortal.cs
@@ -6,8 +6,6 @@ namespace Experiments
     // https://www.codewars.com/kata/59568be9cc15b57637000054
     public static class Immortal
     {
-        //TODO: use int extensions methods for addition
-
         /// set true to enable debug
         public static bool Debug = false;
 
@@ -16,63 +14,56 @@ namespace Experiments
             if (N < 0 || M < 0 || k < 0 || newp < 0)
                 throw new ArgumentException("Negative argument");
 
-            ulong deduction = (ulong)k;
-            ulong mod = (ulong)newp;
-            
-            return (long)EvaluateSumRec(0, (ulong)Math.Max(N, M), (ulong)Math.Min(N, M));
+            var deduction = (ulong) k;
+            var mod = (ulong) newp;
+
+            return (long) EvaluateSumRec(0, (ulong) Math.Max(N, M), (ulong) Math.Min(N, M));
 
             ulong EvaluateSumRec(ulong init, ulong x, ulong y)
             {
                 if (x == 0 || y == 0)
                     return 0;
 
-                if (x == 1 && y == 1)
-                    return init;
-
-                // TODO: lowerPow -> pow2
-                ulong xLowerPow2 = Pow2((int) Math.Log2(x + 1)); // TODO: remove 1
-                ulong yLowerPow2 = Pow2((int) Math.Log2(y + 1));
+                var xPow2 = Pow2((int) Math.Log2(x));
+                var yPow2 = Pow2((int) Math.Log2(y));
 
                 ulong newX;
                 ulong newY;
                 ulong newInit;
                 ulong ans;
 
-                if (xLowerPow2 == yLowerPow2)
+                if (xPow2 == yPow2)
                 {
-                    newX = x - xLowerPow2;
-                    newY = y - xLowerPow2;
-                    newInit = xLowerPow2 ^ yLowerPow2 + init; // NOTE: 64 + 16 = 80
+                    newX = x - xPow2;
+                    newY = y - xPow2;
+                    newInit = init;
 
-                    ans = SumRange(init, xLowerPow2, deduction, mod) * xLowerPow2
-                          + SumRange(init + xLowerPow2, newX, deduction, mod) * xLowerPow2
-                          + SumRange(init + xLowerPow2, newY, deduction, mod) * xLowerPow2;
+                    var sumInRow = SumRange(init + xPow2, xPow2, deduction, mod);
+                    ans = SumRange(init, xPow2, deduction, mod) * xPow2 + sumInRow * newX + sumInRow * newY;
                 }
-                else 
+                else
                 {
-                    newX = x - xLowerPow2;
+                    newX = x - xPow2;
                     newY = y;
-                    newInit = xLowerPow2 + init;
+                    newInit = xPow2 + init;
 
-                    ans = SumRange(init, xLowerPow2, deduction, mod) * y;
+                    ans = SumRange(init, xPow2, deduction, mod) * y;
                 }
 
-                return ans % mod + EvaluateSumRec(
+                return (ans % mod + EvaluateSumRec(
                     newInit,
                     Math.Max(newX, newY),
-                    Math.Min(newX, newY));
+                    Math.Min(newX, newY))) % mod;
             }
 
             [DebuggerStepThrough]
-            static ulong Pow2(int pow) => (ulong)1L << pow;
+            static ulong Pow2(int pow) => (ulong) 1 << pow;
         }
-        
+
         public static ulong SumRange(ulong numFrom, ulong count, ulong deduction, ulong mod)
         {
             if (numFrom >= deduction)
-            {
                 numFrom -= deduction;
-            }
             else
             {
                 count -= deduction - numFrom;
