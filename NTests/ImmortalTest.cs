7464c60ef4d40afc48042e6c8d6078c60b982d35
Alisander Qoshqosh <qalisander@gmail.com>
2021-07-18 18:22:03 +0300

    more tests

--- NTests/ImmortalTest.cs
+++ NTests/ImmortalTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Experiments;
 using FluentAssertions;
 using NUnit.Framework;
@@ -17,6 +16,10 @@ namespace NTests
         [TestCase(1586, 31, 39, 7, 2345)]
         [TestCase(808451, 545, 435, 342, 1000007)]
         [TestCase(5456283, 28_827_050_410L, 35_165_045_587L, 7109602, 13719506)]
+        [TestCase(1, 2, 1, 0, 100500)]
+        [TestCase(3, 3, 1, 0, 100500)]
+        [TestCase(90, 7, 4, 0, 100500)]
+        [TestCase(103, 216, 196, 19, 177)]
         public void ElderAgeTest(long ans, long N, long M, long deduction, long mod)
         {
             Assert.AreEqual(ans, Immortal.ElderAge(N, M, deduction, mod));
@@ -27,6 +30,7 @@ namespace NTests
         [TestCase(5, 14, 10, 5, 1)]      // 0 ... 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 mod 5 = 1   
         [TestCase(5, 14, 11, 7, 0)]      // 0 ... 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 mod 7 = 0
         [TestCase(0, 8, 1, 100, 21)]      // 0 ... 0 + 1 + 2 + 3 + 4 + 5 + 6 mod 100 = 21
+        [TestCase(0, 16, 19, 177, 0)]      // 0 ... 0 + 1 + 2 + 3 + 4 + 5 + 6 mod 100 = 21
         public void SumRangeTest(long numFrom, long count, long deduction, long mod, long expected)
         {
             Immortal.SumRange(numFrom, count, deduction, mod).Should().Be((ulong)expected);
