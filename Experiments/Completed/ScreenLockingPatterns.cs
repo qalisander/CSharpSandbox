c774f2f5592823e43cdc3b3fbec60e8b1e591c52
Alisander Qoshqosh <qalisander@gmail.com>
2021-07-26 21:46:19 +0300

    screen locking patterns: my solution

---
+++ Experiments/Completed/ScreenLockingPatterns.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Experiments.Completed
+{
+    // NOTE: https://www.codewars.com/kata/585894545a8a07255e0002f1/train/csharp
+    public class ScreenLockingPatterns
+    {
+        public static bool[,] DotIsPeeked = new bool[3, 3];
+
+        public static int CountPatternsFrom(char firstDot, int length)
+        {
+            var (x, y) = ParseToIndex(firstDot);
+
+            return CountPatterns(x, y, length);
+        }
+        public static int CountPatterns(int x, int y, int length)
+        {
+            if (length == 1)
+                return 1;
+            if (length == 0)
+                return 0;
+
+            var count = 0;
+
+            DotIsPeeked[x,y] = true;
+
+            for (int x2 = 0; x2 < 3; x2++)
+            {
+                for (int y2 = 0; y2 < 3; y2++)
+                {
+                    if (DotIsPeeked[x2, y2])
+                        continue;
+
+                    if (TryGetMid(x, y, x2, y2, out var midX, out var midY)
+                        && !DotIsPeeked[midX, midY])
+                        continue;
+
+                    count += CountPatterns(x2, y2, length - 1);
+                }
+            }
+
+            DotIsPeeked[x, y] = false;
+
+            return count;
+        }
+
+        public static bool TryGetMid(int x, int y, int x2, int y2, out int midX, out int midY)
+        {
+            var avgX = Math.Abs(x + x2) / 2.0m;
+            var avgY = Math.Abs(y + y2) / 2.0m;
+
+            midX = (int)avgX;
+            midY = (int)avgY;
+
+            return avgX == midX && avgY == midY;
+        }
+
+        public static (int x, int y) ParseToIndex(char dot) => dot switch
+        {
+            'A' => (0, 0),
+            'B' => (1, 0),
+            'C' => (2, 0),
+            'D' => (0, 1),
+            'E' => (1, 1),
+            'F' => (2, 1),
+            'G' => (0, 2),
+            'H' => (1, 2),
+            'I' => (2, 2),
+            _ => (-1, -1)
+        };
+    }
+}
---
+++ NTests/Completed/ScreenLockingPatternsTests.cs
@@ -0,0 +1,22 @@
+﻿using Experiments.Completed;
+using NUnit.Framework;
+
+namespace NTests.Completed
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [Test, Description("Example Tests")]
+        [TestCase('A', 0, ExpectedResult = 0)]
+        [TestCase('A', 10, ExpectedResult = 0)]
+        [TestCase('B', 1, ExpectedResult = 1)]
+        [TestCase('C', 2, ExpectedResult = 5)]
+        [TestCase('D', 3, ExpectedResult = 37)]
+        [TestCase('E', 4, ExpectedResult = 256)]
+        [TestCase('E', 8, ExpectedResult = 23280)]
+        public int ExampleTests(char firstDot, int length)
+        {
+            return ScreenLockingPatterns.CountPatternsFrom(firstDot, length);
+        }
+    }
+}
