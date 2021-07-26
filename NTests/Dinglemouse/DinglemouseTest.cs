41d89b56ee75ed972a41628026b19e1f5503a7b8
Alisander Qoshqosh <qalisander@gmail.com>
2021-07-26 12:33:16 +0300

    trains: test renaiming

--- NTests/Dinglemouse/DinglemouseTest.cs
+++ NTests/Dinglemouse/DinglemouseTest.cs
@@ -9,14 +9,16 @@ namespace NTests
     public class DinglemouseTest
     {
         [Test]
-        [TestCase(3, "KataTrains_SimpleCircle.txt", "Aaaa", 2, "bbbbbbbbbbB", 28, 1000)]
-        [TestCase(5, "KataTrains_SimpleCrossing.txt", "aaaA", 31, "Bbbbbbbbbbb", 7, 1000)]
-        [TestCase(13, "KataTrains_SimpleStation.txt", "Aaaa", 6, "bbbbbbbbbbbbbB", 36, 1000)]
-        [TestCase(516, "KataTrains.txt", "Aaaa", 147, "Bbbbbbbbbbb", 288, 1000)]
-        [TestCase(-1, "KataTrains_NearMissSelf.txt", "aA", 10, "oooooooooooooooooooooO", 70, 200)]
-        [TestCase(-1, "KataTrains_NoCrashOTricky.txt", "aaaA", 15, "bbbB", 5, 100)]
-        [TestCase(0, "KataTrains_Random.txt", "xxxxxxX", 11, "Cccccccccc", 42, 2000)]
-        [TestCase(0, "KataTrains_CrashBeforeStarted.txt", "oO", 10, "oO", 10, 100)]
+        [TestCase(3, "trains_SimpleCircle.txt", "Aaaa", 2, "bbbbbbbbbbB", 28, 1000)]
+        [TestCase(5, "trains_SimpleCrossing.txt", "aaaA", 31, "Bbbbbbbbbbb", 7, 1000)]
+        [TestCase(13, "trains_SimpleStation.txt", "Aaaa", 6, "bbbbbbbbbbbbbB", 36, 1000)]
+        [TestCase(516, "trains.txt", "Aaaa", 147, "Bbbbbbbbbbb", 288, 1000)]
+        [TestCase(-1, "trains_NearMissSelf.txt", "aA", 10, "oooooooooooooooooooooO", 70, 200)]
+        [TestCase(-1, "trains_NoCrashOTricky.txt", "aaaA", 15, "bbbB", 5, 100)]
+        [TestCase(0, "trains_Random.txt", "xxxxxxX", 11, "Cccccccccc", 42, 2000)]
+        [TestCase(0, "trains_CrashBeforeStarted.txt", "oO", 10, "oO", 10, 100)]
+        [TestCase(16, "trains_Limits.txt", "aaaA", 22, "bbbbB", 0, 16)]
+        [TestCase(-1, "trains_NoCrash0Tricky_2.txt", "aaaA", 15, "bbbB", 5, 1000)]
         public void Example(
             int expected, string TestFile, string aTrain, int aTrainPos, string bTrain, int bTrainPos, int limit)
         {
